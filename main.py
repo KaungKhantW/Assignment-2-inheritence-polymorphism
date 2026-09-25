c14fb7964c6cecf07aa346ceeff737b636fb2718
Kaung Khant <khantwinkaung950@gmail.com>
2026-09-25 13:45:58 +0700

    Update main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+# int() converts the user's text input into a whole number
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+result = num1 + num2
+
+print(f"The sum of {num1} and {num2} is: {result}")
+
